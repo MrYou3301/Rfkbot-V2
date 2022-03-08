fef710b7b5156347aba1911007080570217f0cc5
MR YOU 3301 <101115744+MrYou3301@users.noreply.github.com>
2022-03-08 09:50:13 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -3,25 +3,25 @@ let { MessageType } = require('@adiwajshing/baileys')
 let handler = async(m, { conn }) => {
     let kamisato = `
 ┌〔 List Berlangganan 〕
-├ 2 Bulan
-├ 1 Bulan
-├ 1 Minggu
-├ 3 hari
+├ Jadi Owner
+├ Premium Permanent
+├ Sewa 1 Bulan
+├ Sewa Permanent
 └────
 Silahkan klik pada "List Harga" untuk melihat list.
 
 Pembayaran:
-Pulsa (ISAT): 085730903853 (+5.000)
-Gopay: 085730903853
+Pulsa (ISAT): 0857 4234 4873
+Ovo: 0857 4234 4873
 `.trim()
     const button = {
         buttonText: 'List Harga',
         description: kamisato,
         sections:  [{title: "Silahkan di pilih", rows: [
-        {title: '2 Bulan', description: "Rp30.000\nSewa bot tanpa batasan waktu.", rowId:".masuk"},
-        {title: '1 Bulan', description: "Rp20.000\nSewa bot selama 1 bulan.", rowId:".masuk"},
-        {title: '1 Minggu', description: "Rp15.000\nSewa bot selama 1 minggu.", rowId:".masuk"},
-        {title: '3 hari', description: "Rp5.000\nSewa bot selama 3 hari.", rowId:".masuk"},
+        {title: 'Jadi Owner', description: "Rp20.000\nJadi Owner bot tanpa batasan waktu.", rowId:".masuk"},
+        {title: 'Premium Permnnt', description: "Rp10.000\nPremium bot selamanya.", rowId:".masuk"},
+        {title: 'Sewa 1 Bulan', description: "Rp5.000\nSewa bot selama 1 bulan.", rowId:".masuk"},
+        {title: 'Sewa Permanent', description: "Rp10.000\nSewa bot selamanya.", rowId:".masuk"},
         {title: 'Owner', description: "Chat owner nya jika ada perlu.", rowId:".owner"},
         {title: 'Rules', description: "Kebijakan Privasi, Syarat Ketentuan dan Peraturan.", rowId:".rules"},
        ] }],
